a078057e71d4f955d6942f7d5ff826e1e400e5b2
Palmer <ANDREW.PALMER@VIZIENTINC.COM>
2019-08-16 15:37:01 -0500

    checking work with PS

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -59,6 +59,9 @@ public class ControllerServlet extends HttpServlet {
 				case "/insert":
 					insertBook(request, response);
           break;
+				case "/delete":
+					deleteBook(request, response);
+		  break;
         default:
 				   listBooks(request, response);
            break;
@@ -102,6 +105,16 @@ public class ControllerServlet extends HttpServlet {
 		Book newBook = new Book(title, author, Float.parseFloat(priceString));
 
 		bookDAO.insertBook(newBook);
+		response.sendRedirect("list");
+	}
+
+	private void deleteBook(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String idString = request.getParameter("id");
+		Integer id = Integer.parseInt(idString);
+
+		bookDAO.deleteBook(id);
+
 		response.sendRedirect("list");
 	}
 
